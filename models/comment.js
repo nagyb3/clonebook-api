8832de3102799687fd0169c1c16d18b9e49f9bd8
nagyb3 <nagybence2003@gmail.com>
2023-08-20 13:53:55 +0200

    fix /posts and add comments schema

---
+++ models/comment.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const CommentSchema = new mongoose.Schema({
+    comment_author: {
+        type: String,
+        required: true,
+    },
+    text: {
+        type: String,
+        required: true,
+    },
+    post_id: {
+        type: String,
+        required: true,
+    },
+    numberOfLikes: {
+        type: Number,
+        default: 0,
+    },
+});
+
+module.exports = mongoose.model("Comment", CommentSchema);
--- server.js
+++ server.js
@@ -3,12 +3,20 @@ const app = express();
 const mongoose = require("mongoose");
 const asyncHandler = require("express-async-handler");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 
 const PORT = 5000;
 
-const Post = require("./models/Post");
+const Post = require("./models/post");
+const User = require("./models/user");
+const Comment = require("./models/comment");
 
 require("dotenv").config();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.use(express.urlencoded({ extended: false }));
 
 app.listen(PORT, () => {
     console.log(`server started on port ${PORT}`);
@@ -21,21 +29,24 @@ app.get(
 
         console.log(allPosts);
 
-        res.send({});
+        res.send({ all_posts: allPosts });
     })
 );
 
 app.post(
     "/posts",
-    bodyParser.json(),
     asyncHandler(async (req, res) => {
-        const newPost = await new Post();
-        newPost.text = req.body.text;
-        newPost.authorid = req.body.authorid;
-        newPost.save();
+        await Post.create({
+            text: req.body.text,
+            authorid: req.body.authorid,
+        });
+        res.send({
+            message: "message submitted",
+        });
     })
 );
 
+mongoose.set("strictQuery", false);
 const mongoDB = process.env.MONGODB_URL;
 
 async function main() {
