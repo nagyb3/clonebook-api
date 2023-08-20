03dcbd44165dca3773c3b62e6158b1f860c7b2c0
nagyb3 <nagybence2003@gmail.com>
2023-08-20 21:39:42 +0200

    signup and login

--- server.js
+++ server.js
@@ -4,6 +4,11 @@ const mongoose = require("mongoose");
 const asyncHandler = require("express-async-handler");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const bcryptjs = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const session = require("express-session");
 
 const PORT = 5000;
 
@@ -12,6 +17,15 @@ const User = require("./models/user");
 const Comment = require("./models/comment");
 
 require("dotenv").config();
+
+app.use(
+    session({
+        secret: "keyboard cat",
+        resave: false,
+        saveUninitialized: true,
+        cookie: { secure: true },
+    })
+);
 
 app.use(bodyParser.json());
 app.use(cors());
@@ -94,3 +108,69 @@ main()
         }
     })
     .catch((err) => console.log(err));
+
+passport.serializeUser(function (user, done) {
+    done(null, user.id);
+});
+
+passport.deserializeUser(async function (id, done) {
+    try {
+        const user = await User.findById(id);
+        done(null, user);
+    } catch (err) {
+        done(err);
+    }
+});
+
+passport.use(
+    new LocalStrategy(async (username, password, done) => {
+        try {
+            const user = await User.findOne({ username: username });
+            if (!user) {
+                return done(null, false, { message: "Incorrect username" });
+            }
+            bcryptjs.compare(password, user.password, (err, result) => {
+                if (err) throw err;
+                if (result === true) {
+                    return done(null, user);
+                } else {
+                    return done(null, false);
+                }
+            });
+        } catch (err) {
+            return done(err);
+        }
+    })
+);
+
+app.post("/signup", bodyParser.json(), async (req, res) => {
+    if (req.body.username === undefined || req.body.password === undefined) {
+        res.send("No username or password given!");
+    } else {
+        const hashPassword = await bcryptjs.hash(req.body.password, 10);
+        const newUser = new User({
+            username: req.body.username,
+            password: hashPassword,
+            email: req.body.email,
+        });
+        await newUser.save();
+        res.send("Sucess!!");
+    }
+});
+
+app.post(
+    "/login",
+    bodyParser.json(),
+    passport.authenticate("local"),
+    (req, res) => {
+        jwt.sign(
+            { username: req.body.username, password: req.body.password },
+            "secretKey",
+            (err, token) => {
+                res.json({
+                    token,
+                });
+            }
+        );
+    }
+);
